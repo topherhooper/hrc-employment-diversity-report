45c06405134d90def40b9cab4aced860559f68a5
Christopher Hooper <hooper.christopher@gmail.com>
2016-04-02 20:11:52 -0500

    feat(data/api): fill funcs for checking for new data and then retrieving
    Currently, the data is stored as datafiles. The files names hold their update
    date and the api url is hardcoded.

--- ntp/data/api.py
+++ ntp/data/api.py
@@ -1,15 +1,29 @@
 """
-Primary python API for interacting with the data from the open data portal.  There's a lot of cases in here that need to be tested.
+Primary python API for interacting with the data from the open data portal.  There's a lot of cases in here that need
+to be tested.
 """
 import requests
-#from init_db import rdb_data, rdb_timestamps
-from pprint import pprint
-from toolz.itertoolz import groupby, concat
-from toolz.dicttoolz import valmap  
-import time
-import rethinkdb as r
-import datetime
-from collections import Counter
+
+
+def write_csv(path, json_like, fieldnames=None, delimiter=","):
+    """
+    :param path: full path of file to write out
+    :param json_like: json_like to write to csv
+    :param fieldnames: the fields to use as the csv header
+    :param delimiter: defaults to ","
+    :return: None, writes file to path
+    """
+    import csv
+    if not fieldnames:
+        fieldnames = [key for key in json_like[0]]
+    with open(path, 'w') as path:
+        dw = csv.DictWriter(path, delimiter=delimiter, fieldnames=fieldnames)
+        dw.writeheader()
+        for doc in json_like:
+            try:
+                dw.writerow({key: unicode(doc[key]).encode("utf8") for key in doc})
+            except UnicodeEncodeError:
+                print doc
 
 
 def should_update(ntp_last_updated, odp_last_updated):
@@ -17,8 +31,8 @@ def should_update(ntp_last_updated, odp_last_updated):
     Check rethinkdb for a record of imported datasets.
     """
     if int(odp_last_updated) > int(ntp_last_updated):
-        return True 
-    return False 
+        return True
+    return False
 
 
 def check_for_update():
@@ -30,9 +44,8 @@ def check_for_update():
     endpoint = "https://data.nashville.gov/api/views/4ibi-mxs4"
     response = requests.get(endpoint)
     demographics_object = response.json()
-
     epoch_timestamp = demographics_object.get("rowsUpdatedAt")
-    return int(epoch_timestamp)
+    return epoch_timestamp
 
 
 def retrieve_data():
@@ -40,7 +53,7 @@ def retrieve_data():
     Grab the actual demographics data from the Open Data Portal
     """
 
-    api_endpoint  = "https://data.nashville.gov/resource/4ibi-mxs4.json?$limit=50000"
+    api_endpoint = "https://data.nashville.gov/resource/4ibi-mxs4.json?$limit=50000"
     data = requests.get(api_endpoint).json()
     return data
 
@@ -49,5 +62,15 @@ def ntp_last_update():
     """
     Get the last updated value of Inclucivics Data
     """
+    import os
+    def strip_date(date_string):
+        from datetime import datetime
+        return datetime.strptime(date_string, '%Y%m%d')
 
-    return int(1111111111111111) 
+    file_path = 'ntp/files/input/201441207.csv'
+    input_path = os.path.os.path.dirname(os.path.realpath((file_path)))
+    onlyfiles = [
+        strip_date(os.path.splitext(f)[0])
+        for f in os.listdir(input_path)
+        if os.path.isfile(os.path.join(input_path, f))]
+    return max(onlyfiles).strftime('%s')
