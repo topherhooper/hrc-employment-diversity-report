628759b67703e5f8295615e9e00fb80bc6ff38a4
topherhooper <hooper.christopher@gmail.com>
2016-04-05 19:21:17 -0500

    feat(): add celery task

--- ntp/data/celeryconfig.py
+++ ntp/data/celeryconfig.py
@@ -8,13 +8,13 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = "rpc://"
 
 BROKER_IP = os.environ.get("RABBITMQ_PORT_5671_TCP_ADDR", "localhost")
-BROKER_URL="amqp://guest@%s//" % BROKER_IP
+BROKER_URL = "amqp://guest@%s//" % BROKER_IP
 
 CELERY_IMPORTS = ("ntp.data",)
 
 CELERYBEAT_SCHEDULE = {
-        'request-hr-data': {
-            "task": "ntp.data.tasks.get_last_update",
-            "schedule": crontab(0, 0, day_of_week="sat")
-            }   
-    }
+    'request-hr-data': {
+        "task": "ntp.data.tasks.get_last_update",
+        "schedule": crontab(0, 0, day_of_week="sat")
+    },
+}
